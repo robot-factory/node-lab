43d949e3964dee0412388307d028f2101ffd46ed
liuboheng <lbh9311@sina.com>
2020-01-07 22:28:27 +0800

    update

--- scripts/relayNodeCmd3.ts
+++ scripts/relayNodeCmd3.ts
@@ -1,20 +1,23 @@
-import { RelayNode } from './relayNode'
+import { RelayNode } from "./relayNode";
 
 const node1 = new RelayNode(9002);
 
-// setTimeout(() => {
-//   node1.sendMethdRegister(9000,'localhost')
-// }, 2000);
+setTimeout(() => {
+  node1.sendMethdRegister(9000, "47.106.122.155");
+}, 2000);
 
-// setTimeout(() => {
-//   node1.sendMethodGetPeers(9000,'localhost')
-// }, 3000);
+setTimeout(() => {
+  node1.sendMethodGetPeers(9000, "47.106.122.155");
+}, 2500);
 
-setTimeout(()=>{
-  node1.sendMethodPostToPeer(1,9000,'localhost')
-  
-},2000)
+setInterval(() => {
+  node1.sendMethodPostToPeer(1, 9000, "47.106.122.155");
+}, 3000);
 
-// setTimeout(()=>{
+setInterval(() => {
+  node1.sendMethodPing(9000, "47.106.122.155");
+}, 10000);
 
-// })
+setInterval(() => {
+  node1.sendMethodPing(9001, "113.110.200.41");
+}, 10000);
